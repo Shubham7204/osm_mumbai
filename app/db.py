5edd23aac852d71969bd60642c0ff14f92cb4099
Shubham7204 <onlinelearner01learn@gmail.com>
2025-07-20 12:37:19 +0530

    updates

--- app/db.py
+++ app/db.py
@@ -1,10 +1,11 @@
+import os
 import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        dbname="os_mumbai",
-        user="postgres",
-        password="123456",
-        host="localhost",
-        port="5432"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
