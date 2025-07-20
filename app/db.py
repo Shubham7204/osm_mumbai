21e08f02a17d4443594d9f96955d013591dd94d1
Shubham7204 <onlinelearner01learn@gmail.com>
2025-07-20 18:25:56 +0530

    update

--- app/db.py
+++ app/db.py
@@ -1,11 +1,14 @@
 import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        host=os.getenv("DB_HOST"),
+        host=os.getenv("DB_HOST"),  # IPv4 address directly
         port=os.getenv("DB_PORT")
     )
